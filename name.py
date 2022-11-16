3bc7c64501237464e2b39727555f37d125873d2b
Barboss88 <vgrabar.vgg@gmail.com>
2022-11-16 21:41:54 +0200

    changes_2

---
+++ name.py
@@ -0,0 +1,4 @@
+import datetime
+var_date_time=datetime.datetime(2022,11,16,hour=21,minute=35,second=34,microsecond=385)
+print(f'object datetime-{var_date_time}')
+print(f'type-{type(var_date_time)}')
